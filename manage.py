b35dcfaadfa4e3f158c2516a7e4dc22ccf32bea4
SandraRadz <oleksandraradzievska@gmail.com>
2019-08-11 20:57:37 +0300

    changed db initialization

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
-from flask_migrate import MigrateCommand, Migrate
+#from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager
-
+from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
-from app.models import Number, User, db
+from app.models import Number, User
 
 app = create_app()
-db.init_app(app)
+db = SQLAlchemy(app)
 db.create_all()
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-migrate = Migrate(app, db)
+#manager = Manager(app)
+#manager.add_command('db', MigrateCommand)
+# migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
