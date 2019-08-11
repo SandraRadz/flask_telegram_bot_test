ae3a642739b8576c96a48b2f994b0a2f5a11c1e2
SandraRadz <oleksandraradzievska@gmail.com>
2019-08-11 15:15:58 +0300

    requirements.txt

---
+++ bot.py
@@ -0,0 +1,22 @@
+import time
+
+import telebot
+
+from app.models import user_to_db
+
+bot = telebot.TeleBot("984967282:AAH2oy2_VNrUQefzJ9LNCXd2muikSFLPCQU")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_to_db(message.from_user.id)
+    bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name + " " + message.from_user.last_name)
+
+
+while True:
+    try:
+        bot.polling(none_stop=True, interval=1)
+
+    except Exception as e:
+        telebot.logger.error(e)
+        time.sleep(15)
