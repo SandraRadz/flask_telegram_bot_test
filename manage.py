6f5b47ee68d0f71e5162e7be2c647aa8bea95ee9
SandraRadz <oleksandraradzievska@gmail.com>
2019-08-11 19:56:34 +0000

    init migrations

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager
-
+import telebot
+import request
 
 from app import create_app
 from app.models import Number, User, db
+from bot import bot
 
 app = create_app()
 db.init_app(app)
@@ -11,6 +13,13 @@ db.init_app(app)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 migrate = Migrate(app, db)
+
+secret = "GUID"
+@app.route('/{}'.format(secret), methods=["POST"])
+def webhook():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    print("Message")
+    return "ok", 200
 
 if __name__ == '__main__':
     manager.run()
